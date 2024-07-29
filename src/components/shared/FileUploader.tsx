051b959a0c328987a5444bde344bd26dcec6245e
Yash Bhalgat <yashbhalgat@Yashs-Air.lan>
2024-07-29 13:08:57 -0700

    final fixes

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react'
+import {useCallback, useState} from 'react'
 import {useDropzone, FileWithPath} from 'react-dropzone'
 import { Button } from '../ui/button'
 
